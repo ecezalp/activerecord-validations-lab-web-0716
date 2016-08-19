d8865ab3ee9d44287be111aa0129df28cf668b22
Ece Ozalp <github email address>
2016-08-19 15:07:26 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 10}
+  validates :summary, length: {maximum: 100}
+  validates :category, inclusion: { in: %w(Fiction fiction Nonfiction nonfiction)}
+  validate :clickbait_validate
+
+ private
+
+ def clickbait_validate
+
+  if title
+    if !self.title.downcase.include?((("won't believe" || "secret") || "top /top[0-9]*/") || "guess")
+      errors.add(:title, "m8, clickb8!")
+    end
+  end
+ end
+
 end
+
